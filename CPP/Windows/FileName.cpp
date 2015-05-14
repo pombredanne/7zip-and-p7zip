b50a6d43caaf871542767bb2e88eb6c60655ed1e
Philippe Ombredanne <pombredanne@nexb.com>
2015-05-14 20:42:41 +0200

    Add resolution of dot folders back from upstream 7z.
     * added back ResolveDotsFolders, ensuring paths are using POSIX
       separators.
     * updated GetFullPath to call ResolveDotsFolders
     * enhanced IsAbsolutePath to handle both POSIX and Win paths

--- CPP/Windows/FileName.cpp
+++ CPP/Windows/FileName.cpp
@@ -16,7 +16,7 @@ namespace NWindows {
 namespace NFile {
 namespace NName {
 
-bool IsAbsolutePath(const wchar_t *s) { return s[0] == WCHAR_PATH_SEPARATOR; } // FIXME
+bool IsAbsolutePath(const wchar_t *s) { return s[0] == WCHAR_PATH_SEPARATOR || s[0] == WIN_WCHAR_PATH_SEPARATOR; }
 
 
 void NormalizeDirPathPrefix(CSysString &dirPath)
@@ -37,11 +37,79 @@ void NormalizeDirPathPrefix(UString &dirPath)
 }
 #endif
 
+
+static bool ResolveDotsFolders(UString &s)
+{
+  s.Replace(L'\\', WCHAR_PATH_SEPARATOR);
+  for (int i = 0;;)
+  {
+    wchar_t c = s[i];
+    if (c == 0)
+      return true;
+    if (c == '.' && (i == 0 || s[i - 1] == WCHAR_PATH_SEPARATOR))
+    {
+      wchar_t c1 = s[i + 1];
+      if (c1 == '.')
+      {
+        wchar_t c2 = s[i + 2];
+        if (c2 == WCHAR_PATH_SEPARATOR || c2 == 0)
+        {
+          if (i == 0)
+            return false;
+          int k = i - 2;
+          for (; k >= 0; k--)
+            if (s[k] == WCHAR_PATH_SEPARATOR)
+              break;
+          unsigned num;
+          if (k >= 0)
+          {
+            num = i + 2 - k;
+            i = k;
+          }
+          else
+          {
+            num = (c2 == 0 ? (i + 2) : (i + 3));
+            i = 0;
+          }
+          s.Delete(i, num);
+          continue;
+        }
+      }
+      else
+      {
+        if (c1 == WCHAR_PATH_SEPARATOR || c1 == 0)
+        {
+          unsigned num = 2;
+          if (i != 0)
+            i--;
+          else if (c1 == 0)
+            num = 1;
+          s.Delete(i, num);
+          continue;
+        }
+      }
+    }
+    i++;
+  }
+}
+
+
 bool GetFullPath(CFSTR dirPrefix, CFSTR s, FString &res)
 {
-	res = FString(dirPrefix) + FString(s);
+  res = FString(dirPrefix)
+  resolved = s;
 
-	return true;
+  UString rem = fs2us(s);
+  if (!ResolveDotsFolders(rem)) 
+  {
+    res += FString(s);
+  }
+  else 
+  {
+    res += us2fs(rem);
+  }
+  return true;
 }
+
 
 }}}
