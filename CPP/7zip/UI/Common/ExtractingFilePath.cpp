450eaed3872604d5403b37f6dd1344cdbc357433
Philippe Ombredanne <pombredanne@nexb.com>
2015-05-15 14:05:45 +0200

    Make functions static

--- CPP/7zip/UI/Common/ExtractingFilePath.cpp
+++ CPP/7zip/UI/Common/ExtractingFilePath.cpp
@@ -147,7 +147,7 @@ UString MakePathNameFromParts(const UStringVector &parts)
   return result;
 }
 
-void RemoveDotParts(const UStringVector &parts, UStringVector &cleanParts)
+static void RemoveDotParts(const UStringVector &parts, UStringVector &cleanParts)
 {
   FOR_VECTOR (i, parts)
   {
@@ -162,7 +162,7 @@ void RemoveDotParts(const UStringVector &parts, UStringVector &cleanParts)
 }
 
 
-void RemoveDotParts2(UStringVector &parts)
+static void RemoveDotParts2(UStringVector &parts)
 {
   for (unsigned i = parts.Size() - 1; i >= 0; i--)
   {
