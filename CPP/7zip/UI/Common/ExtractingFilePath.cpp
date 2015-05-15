918c0b9eced58a2cea1865cb74188cddb98c1bcb
Philippe Ombredanne <pombredanne@nexb.com>
2015-05-15 13:53:39 +0200

    More tracing

--- CPP/7zip/UI/Common/ExtractingFilePath.cpp
+++ CPP/7zip/UI/Common/ExtractingFilePath.cpp
@@ -136,6 +136,7 @@ void MakeCorrectPath(bool isPathFromRoot, UStringVector &pathParts, bool replace
 
 UString MakePathNameFromParts(const UStringVector &parts)
 {
+  printf("##DBG CPP/7zip/UI/Common/ExtractingFilePath.cpp::MakePathNameFromParts\n");
   UString result;
   FOR_VECTOR (i, parts)
   {
