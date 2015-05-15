b2f1b3cea681d9482d45c6a8518cd31dde180c21
Philippe Ombredanne <pombredanne@nexb.com>
2015-05-15 13:15:46 +0200

    Pointer and reference fixup

--- CPP/7zip/UI/Common/ExtractingFilePath.cpp
+++ CPP/7zip/UI/Common/ExtractingFilePath.cpp
@@ -148,14 +148,13 @@ UString MakePathNameFromParts(const UStringVector &parts)
 
 void RemoveDotParts(const UStringVector &parts, const UStringVector &cleanParts)
 {
-  UString s;
   FOR_VECTOR (i, parts)
   {
     printf("##DBG CPP/7zip/UI/Common/ExtractingFilePath.cpp::RemoveDotParts: candidate: parts[%d] = '%ls'\n", i, (const wchar_t *) parts[i]);
     if (!s.IsEmpty() && s != L".." && s != L"." && s != L" ")
     {
       printf("##DBG CPP/7zip/UI/Common/ExtractingFilePath.cpp::RemoveDotParts: keeping: parts[%d] = '%ls'\n", i, (const wchar_t *) parts[i]);
-      s = parts[i];
+      const UString &s = parts[i];
       cleanParts.Add(s);
     }
   }
